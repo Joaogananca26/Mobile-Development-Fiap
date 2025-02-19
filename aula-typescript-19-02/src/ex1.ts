553848f64e694a390ac05e458b4fda5985fd813c
joaogananca26 <joaogananca26@gmail.com>
2025-02-19 20:43:08 -0300

    Exercício 1 feito!

---
+++ aula-typescript-19-02/src/ex1.ts
@@ -0,0 +1,5 @@
+// Crie um array de números e imprima a soma dos valores.
+
+let valores: number[] = [10, 20, 30];
+let somaValores: number = valores.reduce((acc, val) => acc + val, 0);
+console.log("Soma dos valores:", somaValores);
