ca9abe616cc362f9f04adfeb56e588821d3b2630
Joaogananca26 <joaogananca26@gmail.com>
2025-02-20 14:17:32 -0300

    Exercício 1A de Estruturas de Controle feito!

---
+++ aula-javascript-13-02/estruturas-de-controle.js
@@ -0,0 +1,14 @@
+// Exercício 1
+
+// Exercício A
+let numero;
+
+numero = prompt("Escolha um número e irei te falar se é positivo, negativo ou se ele é zero!")
+
+if (numero > 0) {
+    console.log("O número é positivo.");
+} else if (numero < 0) {
+    console.log("O número é negativo.");
+} else {
+    console.log("O número é zero.");
+}
