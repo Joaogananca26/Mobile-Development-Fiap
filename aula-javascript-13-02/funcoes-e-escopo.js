7c309326a483b9e31db4c63a70381c0d3a1f1e25
Joaogananca26 <joaogananca26@gmail.com>
2025-02-20 14:20:35 -0300

    Exercício 1A de Funções e Escopo feito!

---
+++ aula-javascript-13-02/funcoes-e-escopo.js
@@ -0,0 +1,9 @@
+// Exercício 1A
+
+function converterParaMaiusculas(string) {
+    return string.toUpperCase();
+}
+
+let testar = "teste";
+let resultado = converterParaMaiusculas(testar)
+console.log(resultado)
