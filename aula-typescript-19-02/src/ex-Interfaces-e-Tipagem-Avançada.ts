e142a106b2bc558fcd346a8a617e1e747af46359
joaogananca26 <joaogananca26@gmail.com>
2025-02-20 22:21:05 -0300

    Exercício 1A de Interfaces e Tipagem Avançada feito!

---
+++ aula-typescript-19-02/src/ex-Interfaces-e-Tipagem-Avançada.ts
@@ -0,0 +1,22 @@
+// Exercício 1A
+// Crie uma interface chamada Carro, que contenha as seguintes propriedades: marca (string) modelo (string) ano (number) motor (opcional, string)
+
+/*
+interface Carro {
+    marca: string;
+    modelo: string;
+    ano: number;
+    motor?: string;
+}
+
+const meuCarro: Carro = {
+    marca: "Toyota",
+    modelo: "Corolla",
+    ano: 2022
+};
+
+console.log("Meu Carro:");
+console.log(`Marca: ${meuCarro.marca}`);
+console.log(`Modelo: ${meuCarro.modelo}`);
+console.log(`Ano: ${meuCarro.ano}`);
+*/
