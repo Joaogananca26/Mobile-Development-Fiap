6b6b8695aefeb538e356b0faf9d82a9a10018dca
joaogananca26 <joaogananca26@gmail.com>
2025-02-26 19:50:19 -0300

    Testando comandos de moto e carro no arquivo index.ts

---
+++ aula-avancada-typescript-26-02/src/index.ts
@@ -0,0 +1,21 @@
+import { Carro } from "./interfaces/Carro";
+import { Moto } from "./interfaces/Moto";
+
+const meuCarro: Carro = {
+    marca: "Chevrolet",
+    modelo: "Tracker",
+    ano: 2019,
+    portas: 4,
+    acelerar: () => "O carro está acelerando!"
+};
+const minhaMoto: Moto = {
+    marca: "Homda",
+    modelo: "Biz 125",
+    ano: 2024,
+    cilindradas: 120,
+    acelerar: () => "A moto está acelerando!"
+};
+console.log("Carro:", meuCarro);
+console.log(meuCarro.acelerar());
+console.log("Moto:", minhaMoto);
+console.log(minhaMoto.acelerar());
